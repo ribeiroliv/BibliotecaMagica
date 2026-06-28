7e0ef8f051afce8d0b15fde25014ce40a7a8cac5
Iago Oliveira <iagooliveiracontato@gmail.com>
2026-06-28 11:28:17 -0300

    Update criptografia.c
    Implementa funções de criptografia e descriptografia

---
+++ criptografia.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void criptografar(char* str){
+
+    int i;
+
+    for(i=0;str[i]!='\0';i++){
+
+        str[i]= (char)(255 - (unsigned char)str[i]);
+    }
+}
+
+void descriptografar(char *str){
+
+    int i;
+
+    for(i = 0; str[i] != '\0'; i++){
+
+        str[i] = (char)(255 - (unsigned char)str[i]);
+    }
+}
