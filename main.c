53ea2679c17e253ac04f4b5a952813e0accd757a
monarasoares <monarasoares@discente.ufg>
2026-06-28 16:32:27 -0300

    primeira parte

---
+++ main.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Data {
+    int dia;
+    int mes;
+    int ano;
+}; 
+
+struct Autor {
+    char nome[128];
+    struct Data data_nascimento;
+};
+
+struct LivroMagico { 
+    unsigned int ID; 
+    char titulo[200];
+    struct Autor autor;
+    struct Data data_escrita;
+};
