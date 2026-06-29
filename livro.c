51df7a28243c7a90525b8641a505043f0d4c9f58
michele costa <michelecostadevv@gmail.com>
2026-06-29 19:41:04 -0300

    Implementa deleção de livro

--- livro.c
+++ livro.c
@@ -4,8 +4,9 @@
 #include "estruturas.h"
 
 #define TAM 100
+
 /*Essa função auxilia na busca dos livros por id*/
-LivroMagico * buscaId( LivroMagico **biblioteca, int id) {
+LivroMagico * buscaId(LivroMagico **biblioteca, int id) {
 	int i;
 	for(i = 0; i < TAM; i++) {
 		if(biblioteca[i] != NULL) {
@@ -27,14 +28,15 @@ void mostrarLivro(LivroMagico **biblioteca, int id) {
 		printf("ID: %d\n", livro->id);
 		printf("Titulo: %s\n", livro->titulo);
 		printf("Autor: %s\n", livro->autor.nome);
-		printf("Data: %d/%d/%d\n", livro->data_escrita.dia, livro->data_escrita.mes, livro->data_escrita.ano);	
+		printf("Data de Nascimento do Autor: %d/%d/%d\n", livro->data_nascimento.dia, livro->data_nascimento.mes, livro->data_nascimento.ano);
+		printf("Data de Publicação: %d/%d/%d\n", livro->data_escrita.dia, livro->data_escrita.mes, livro->data_escrita.ano);	
 	} else {
 		printf("Livro nao encontrado!\n");
 	}
 }
 
 /*Essa função imprime a lista de todos os livros da biblioteca e seus respectivos IDs*/
-void listarTitulos( LivroMagico **biblioteca) {
+void listarTitulos(LivroMagico **biblioteca) {
 	int i;
 	int bibliotecaVazia = 0;
 
@@ -48,4 +50,27 @@ void listarTitulos( LivroMagico **biblioteca) {
 	if(bibliotecaVazia == 0) {
 		printf("Ainda não há livros nessa biblioteca!\n");
 	}
+}
+
+/*Essa função deleta um livro da biblioteca*/
+void deletarLivro(livroMagico **biblioteca, int id) {
+	int i;
+	int encontrouLivro = 0;
+
+	for(i = 0; i < TAM; i++) {
+		if(biblioteca[i] != NULL) {
+			if(biblioteca[i]->id == id) {
+				encontrouLivro = 1;
+				break;
+			}
+		} 
+	}
+
+	if(encontrouLivro != 0) {
+		free(biblioteca[i]);
+		biblioteca[i] = NULL;
+	} else {
+		printf("Não há livro a ser deletado!\n");
+	}
+	
 }
--- livro.h
+++ livro.h
@@ -1,3 +1,4 @@
 LivroMagico * buscaId( LivroMagico **biblioteca, int id);
 void mostrarLivro(LivroMagico **biblioteca, int id);
-void listarTitulos( LivroMagico **biblioteca);
+void listarTitulos(LivroMagico **biblioteca);
+void deletarLivro(livroMagico **biblioteca, int id);
